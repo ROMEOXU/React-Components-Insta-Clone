92051cfc5768ef5e2ee2a72144e37d656c59a8f2
Mac <xuromeo@gmail.com>
2020-04-14 19:16:46 -0700

    the add button

--- src/components/PostsContainer/LikeSection.js
+++ src/components/PostsContainer/LikeSection.js
@@ -1,21 +1,22 @@
-import React from 'react';
+import React ,{useState} from 'react';
 
 const LikeSection = props => {
-  return (
+ const [count,setCount] = useState(0);
+ return (
 <div>
     <div
       className="like-section"
       key="likes-icons-container"
     >
       <div className="like-section-wrapper">
-        <i className="far fa-heart" />
+        <i className="far fa-heart" onClick={()=>setCount(count+1)}/>
       </div>
       <div className="like-section-wrapper">
         <i className="far fa-comment" />
       </div>
     </div>
     <p className="like-number">
-      {props.like} likes
+      {props.like + count }likes
       
     </p>
 </div>
